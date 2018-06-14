255d0ef1dee2fa936d25ad7fa2146d69a7c2dcb2
Shay Bergmann <shaybergmann@gmail.com>
2018-06-13 21:31:45 -0400

    🐟 add module

---
+++ modules/programs/fish.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+let
+  cfg = config.programs.fish;
+in {
+  programs.fish = {
+    enable = true;
+    promptInit = ''
+      set -g default_user kreisys
+      set -g theme_color_scheme base16-dark
+      set -g theme_nerd_fonts yes
+    '';
+
+    shellInit = ''
+      set -gx PATH $HOME/.local/bin $PATH
+      fish_vi_key_bindings
+      function fish_user_key_bindings
+        for mode in insert default visual
+          bind -M $mode \cf forward-char
+          bind -M $mode \ca beginning-of-line
+          bind -M $mode \cx end-of-line
+        end
+      end
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [ iterm2-integration bobthefish docker-completions git ];
+
+  environment.shells = [
+    "/run/current-system/sw/bin/fish"
+    "/var/run/current-system/sw/bin/fish"
+    "${pkgs.fish}/bin/fish"
+  ];
+
+}
